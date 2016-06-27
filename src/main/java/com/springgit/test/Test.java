2a6c72dab5834f6958776d2bdda729fbdb5d2a84
bruce <sohryong@gmail.com>
2016-06-27 17:32:55 +0900

    Add Test.java printHello Method

---
+++ src/main/java/com/springgit/test/Test.java
@@ -0,0 +1,16 @@
+package com.springgit.test;
+
+public class Test {
+
+	public Test() {
+		printTest();
+	}
+	
+	private void printTest() {
+		System.out.println("Spring GIT Test!");
+	}
+	
+	private void printHello() {
+		System.out.println("Hello");
+	}
+}
